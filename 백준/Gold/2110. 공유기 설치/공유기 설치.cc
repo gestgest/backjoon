7af45b0ea5d7b90b02d33d88f4cc0649a6874c8f
gestgest <87427692+gestgest@users.noreply.github.com>
2023-09-03 14:34:17 +0900

    [Gold IV] Title: 공유기 설치, Time: 32 ms, Memory: 3688 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.cc
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.cc
@@ -11,30 +11,14 @@ int indexSearch(vector<int>& house, int dis, int c, int max)
 	int sum = house[0];
 	int i;
 	int count = 1;
-	int min = 0;
 	sum += dis;
-	
+
 	for (i = 1; i < house.size(); i++)
 	{
-		// = 
-		if (sum == house[i])
+		//간소화[★]
+		if (sum <= house[i])
 		{
-			sum += dis;
-			count++;
-			min = dis;
-		}
-		else if (sum < house[i]) {
-			int num = house[i] - sum;
-			num += dis;
-			if (0 == min) {
-				min = num;
-			}
-			else if (num < min) {
-				min = num;
-			}
-
 			sum = house[i];
-
 			sum += dis;
 			count++;
 		}
@@ -42,40 +26,30 @@ int indexSearch(vector<int>& house, int dis, int c, int max)
 	//
 	if (count < c)
 		return -1;
-	return min;
+	return dis;
 }
 
 
-int wife_binary_search(vector<int> &house,int start, int end, const int max, const int c)
+int wife_binary_search(vector<int>& house, int start, int end, const int max, const int c)
 {
-	int mid = (start + end) / 2;
-	if (1 == end)
-	{
-		return 1;
-	}
-
+	//이것을 맨 앞에 넣었어야 함[★]
+	//이런 배열탐색전 조건문은 맨 앞에
 	if (end < start) {
 		return 0;
 	}
+	int mid = (start + end) / 2;
 	int jud = indexSearch(house, mid, c, max);
 
 
-	//무조건 앞으로 넘겨야 할듯
-	//단, return에서 if로 큰 값 비교
+	// dis값이 큰 경우
 	if (jud == -1) {
-		if (end <= start) {
-			return 0;
-		}
-		return wife_binary_search(house, start, mid, max, c);
+		return wife_binary_search(house, start, mid - 1, max, c);
 	}
 	else {
 		int num = wife_binary_search(house, mid + 1, end, max, c);
 		return (num > jud) ? num : jud;
 	}
 
-	//대충 함수 마지막 함수 인덱스 출력 또는 -1
-	// -1이면 넘친다. = end값을 mid
-	// else start값을 mid + 1
 }
 
 int main()
@@ -96,7 +70,7 @@ int main()
 
 	int min = house[0];
 	int max = house[n - 1];
-	num = (max - min) / (c - 1);
+	num = (max - min) / (c - 1); //이 값을 넘을 수 없음 [기존의 탑]
 
 
 	//간격으로 이분탐색하는게 포인트
@@ -105,27 +79,3 @@ int main()
 	//1~num
 }
 
-/*
-5 3
-1 2 8 4 9
-=> 3
-
-5 2
-1 2 3 9 22
-=> 21
-
-5 3
-1 2 3 9 22
-=>8
-
-6 4
-1 2 4 8 9 11
-
-10 3
-1 2 3 6 40 68 88 90 140 203
-=> 89
-
-10 4
-1 10 21 33 34 35 36 80 98 121
-=> 35
-*/
