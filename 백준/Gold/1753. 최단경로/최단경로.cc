9ce42794e2000fda07e5c17d4ea44641832f3e68
gestgest <87427692+gestgest@users.noreply.github.com>
2024-09-08 19:39:06 +0900

    [Gold IV] Title: 최단경로, Time: 96 ms, Memory: 15492 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.cc
+++ 백준/Gold/1753. 최단경로/최단경로.cc
@@ -2,84 +2,75 @@
 #include <vector>
 #include <queue>
 #include <algorithm>
-#include <functional>
-
-#define MAX_VERTEX_SIZE 20001
-#define INF 2147483647
 
 using namespace std;
 
+#define INF 2147463647
 
-int score[MAX_VERTEX_SIZE];
-
+//dj
 
 class Edge {
 public:
 	int dst;
 	int value;
-	Edge() {
-
-	}
 	Edge(int dst, int value) {
 		this->dst = dst;
 		this->value = value;
 	}
-	//오름차순
-	bool operator < (const Edge & edge) const
+
+	const bool operator <(const Edge& edge) const&
 	{
-		//유지가 false, change가 true
 		return this->value > edge.value;
 	}
 };
 
-
-//우선순위에 나오는 값 출력, 단 이미 score가 고정된 edge는 제외
-Edge getMinEdge(priority_queue<Edge> &pq)
+Edge getEdge(priority_queue<Edge> & edge_queue, vector<int> & short_path)
 {
 	while (true)
 	{
-		if (pq.empty())
-		{
+		if (edge_queue.empty()) {
 			return Edge(-1, -1);
 		}
-		Edge edge = pq.top();
-		pq.pop();
+		Edge edge = edge_queue.top();
+		edge_queue.pop();
 
-		if (score[edge.dst] == INF)
-		{
-			score[edge.dst] = edge.value;
+		if (short_path[edge.dst] == INF) {
+			short_path[edge.dst] = edge.value;
 
 			return edge;
 		}
 	}
 }
 
-void dj(vector<vector<Edge>> &graph, int start, int size)
-{
-	int index = start;
-	score[start] = 0;
 
-	priority_queue<Edge> graph_pq;
+void bfs(vector<Edge> *edges, int v, int start)
+{
+	vector<int> short_path = vector<int> (v, INF);
+	priority_queue<Edge> bfs_queue;
+
+	bfs_queue.push(Edge(start, 0));
 
 	while (true)
 	{
-		//graph 에 있는 edge들 pq에 넣기
-		for (int i = 0; i < graph[index].size(); i++)
-		{
-			Edge tmp = graph[index][i];
-			Edge edge(tmp.dst, score[index] + tmp.value);
+		Edge edge = getEdge(bfs_queue, short_path);
 
-			//그러고
-			graph_pq.push(edge);
-		}
-		
-		Edge edge = getMinEdge(graph_pq);
-		
 		if (edge.dst == -1) {
 			break;
 		}
-		index = edge.dst;
 		
+		for (int i = 0; i < edges[edge.dst].size(); i++)
+		{
+			bfs_queue.push(Edge(edges[edge.dst][i].dst, edges[edge.dst][i].value + edge.value));
+		}
+	}
+
+	for (int i = 0; i < v; i++)
+	{
+		if (INF != short_path[i])
+			cout << short_path[i] << '\n';
+		else {
+			cout << "INF" << '\n';
+		}
 	}
 }
 
@@ -88,46 +79,22 @@ int main()
 	cin.tie(NULL);
 	ios_base::sync_with_stdio(false);
 
-	
 	int v, e;
 	int start;
+	int a, b, c;
 
+	vector<Edge> edges[20001];
 
 	cin >> v >> e;
-	cin >> start;
-	start--;
 
-	for (int i = 0; i < v; i++)
-	{
-		score[i] = INF; //INF라는 뜻
-	}
-
-	vector<vector<Edge>> graph = vector<vector<Edge>>(v);
+	cin >> start; start--;
 	
-
-	//우선순위 큐
 	for (int i = 0; i < e; i++)
 	{
-		int src, dst, w;
-		cin >> src >> dst >> w; //u -> v [w]
-
-		src--; dst--;
-		
-		graph[src].push_back(Edge(dst, w));
+		cin >> a >> b >> c;
+		a--; b--;
+		edges[a].push_back(Edge (b, c));
 	}
 
-	//맨 처음의 우선순위 큐만 pop, 이후 그 도착 인덱스의 경로를 확장
-	// [ 자신의 score + 도착의 w ]
-	//우선순위 큐 추가
-	// 맨 처음 큐만 제거, isConst 고정
-	dj(graph, start,v);
-
-	for (int i = 0; i < v; i++)
-	{
-		if(score[i] != INF)
-			cout << score[i] << '\n';
-		else {
-			cout << "INF" << '\n';
-		}
-	}
+	bfs(edges, v, start);
 }
