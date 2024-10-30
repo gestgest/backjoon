240c559e2db1e75907f8695485cc671b6196358a
gestgest <87427692+gestgest@users.noreply.github.com>
2024-10-30 22:15:38 +0900

    [Platinum V] Title: 박성원, Time: 448 ms, Memory: 27624 KB -BaekjoonHub

--- 백준/Platinum/1086. 박성원/박성원.cc
+++ 백준/Platinum/1086. 박성원/박성원.cc
@@ -6,100 +6,129 @@
 #define MAX_STRING_SIZE 51
 
 using namespace std;
-typedef long long ll;
-ll n, k;
+
+int n, k;
+long long base = 1;
+long long dp[1 << 15][100];
 string s_tmps[15];
+int value_string[15]; //나머지 값
 int cache[MAX_STRING_SIZE];
-int value_string[15];
-// 사용한 원소를 비트마스킹
-// 나머지에 대해 저장
-ll dp[1 << 15][100];
 
 
-
-// 최대 공약수(기약분수로 만들기 위함)
-ll GCD(ll a, ll b) {
-    return b ? GCD(b, a % b) : a;
-}
-
-//맞음
-long long stringToValue(string s)
+//문자열을 나머지 값으로
+long long stringToValue(string& s)
 {
-    long long sum = 0;
-    for (int i = 0; i < s.size(); i++)
-    {
-        sum *= 10;
-        sum += s[i] - '0';
-        sum %= k;
-    }
-    return sum;
+	long long sum = 0;
+	for (int i = 0; i < s.size(); i++)
+	{
+		long long num1 = (s[i] - '0') % k;
+		long long num2 = (cache[s.size() - 1 - i]); //캐시값 자체가 나누기 한 값
+
+		sum += num1 * num2 % k;
+		sum %= k;
+		//만약 123 456이라면? => for문 j로 나누고 
+		//((1 % k) * (100000 % k => 그 cache값으로 구분)) % k;
+	}
+	return sum;
 }
 
-//맞음
 long long dp_park(int value, int status)
 {
-    //long long num = stringToValue(s);
+	//long long num = stringToValue(s);
 
-    if (status == (1 << n) - 1)
-    {
-        return value == 0 ? 1 : 0; //나머지 값이 0이면 1, 나머지면 0
-    }
-    //나머지가 [101][2]인 애는 결국 똑같은 값이 나온다
+	if (status == (1 << n) - 1) 
+	{
+		return value == 0 ? 1 : 0; //나머지 값이 0이면 1, 나머지면 0
+	}
+	//나머지가 [101][2]인 애는 결국 똑같은 값이 나온다
 
-    //한번이라도 지나갔다면 
-    if (dp[status][value] != -1) {
-        return dp[status][value];
-    }
+	//한번이라도 지나갔다면 
+	if (dp[status][value] != -1) {
+		return dp[status][value];
+	}
 
-    dp[status][value] = 0; // 지나간 길 설정
+	dp[status][value] = 0; // 지나간 길 설정
 
 
-    for (int i = 0; i < n; i++)
-    {
-        //이미 있다면
-        if (status & (1 << i)) {
-            continue;
-        }
-        //문자열 합체
+	for (int i = 0; i < n; i++)
+	{
+		//이미 있다면
+		if (status & (1 << i)) {
+			continue;
+		}
+		//문자열 합체
 
-        //value는 이미 k를 나눈 나머지 값
-        int next_value = (value * cache[s_tmps[i].size()] % k + value_string[i]) % k;
-        //7 + 008이라고 가정하면
-        //value * cache[s_tmps[i].size()]는 (7 * (1000 % k))
-        //+ value_string[i]는 8
+		//value는 이미 k를 나눈 나머지 값
+		int next_value = (value * cache[s_tmps[i].size()] % k + value_string[i]) % k;
+		//7 + 008이라고 가정하면
+		//value * cache[s_tmps[i].size()]는 (7 * (1000 % k))
+		//+ value_string[i]는 8
 
-        dp[status][value] += dp_park(next_value, status | (1 << i));
-    }
+		dp[status][value] += dp_park(next_value, status | (1 << i));
+	}
 
-    return dp[status][value];
+	return dp[status][value];
 }
 
-int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cin >> n;
-    for (ll i = 0; i < n; i++)
-        cin >> s_tmps[i];
-    cin >> k;
-    for (int i = 0; i < (1 << n); i++)
-    {
-        for (int j = 0; j < k; j++)
-        {
-            dp[i][j] = -1;
-        }
-    }
-    cache[0] = 1 % k;
-    for (int i = 1; i < 51; i++)
-        cache[i] = (cache[i - 1] * 10) % k;
-    for (int i = 0; i < n; i++)
-        value_string[i] = stringToValue(s_tmps[i]);
-    // 집합의 모든 원소를 사용, 나머지가 0인 경우의 수 cnt
-    // 전체 경우의 수(펙토리얼) all_cnt
-    ll all_cnt = 1, cnt = dp_park(0, 0);
-    for (int i = 1; i <= n; i++)
-        all_cnt *= i;
-    //최대 공약수
-    ll gcd = GCD(cnt, all_cnt);
-    cout << cnt / gcd << '/' << all_cnt / gcd;
-    return 0;
+//최대공약수
+long long gcd(long long a, long long b)
+{
+	if (b == 0) {
+		return a;
+	}
+
+	return gcd(b, a % b);
+}
+
+//모든 자릿수의 나머지 값을 수집
+void makeCahce()
+{
+	int result = 1;
+	for (int i = 0; i < MAX_STRING_SIZE; i++)
+	{
+		cache[i] = result;
+		result *= 10;
+		result %= k;
+		//i = 0, i + 1번째 자리
+	}
+}
+
+int main()
+{
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	int status = 0;
+	cin >> n;
+
+	for (long long i = 0; i < n; i++)
+	{
+		cin >> s_tmps[i];
+		base *= i + 1;
+	}
+
+	cin >> k;
+	makeCahce();
+
+	for (int i = 0; i < (1 << n); i++)
+	{
+		for (int j = 0; j < k; j++)
+		{
+			dp[i][j] = -1;
+		}
+	}
+	for (int i = 0; i < n; i++)
+	{
+		value_string[i] = stringToValue(s_tmps[i]); //나머지 값
+	}
+
+
+	long long num = dp_park(0, 0);
+	if (num == 0) {
+		cout << "0/1" << '\n';
+	}
+	else {
+		long long gg = gcd(num, base);
+		cout << num / gg << "/" << base / gg << '\n';
+	}
 }
