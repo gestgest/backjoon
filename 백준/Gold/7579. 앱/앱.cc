e3793d2b0d246f571dca4f20b4cc30562e0e93e0
gestgest <87427692+gestgest@users.noreply.github.com>
2025-02-25 23:34:28 +0900

    [Gold III] Title: 앱, Time: 0 ms, Memory: 2180 KB -BaekjoonHub

--- 백준/Gold/7579. 앱/앱.cc
+++ 백준/Gold/7579. 앱/앱.cc
@@ -4,59 +4,65 @@
 
 using namespace std;
 
-int dp[101][10001] = { 0, };
+struct Process {
+	int memory;
+	int cost;
+};
 
-int max(int a, int b) {
-	if (a < b) {
-		return b;
-	}
-	return a;
-}
-
-// n + n + (n * cost) + cost
 int main()
 {
 	cin.tie(NULL);
 	ios_base::sync_with_stdio(false);
 
-	int n, m, num, sum = 0;
-	int i, j;
+	//DP
+
+	//n은 앱 갯수, m은 얻어야 하는 메모리 갯수
+	int n, m;
+	vector<Process> process_list;
+	vector<int> dp;
+	int sum = 0;
+
+	//c는 오른쪽
+	//값은 메모리값? [메모리 값이 최대한 커야함]
+	//예를 들어 c = 3이고 메모리는 20이면
+	//0 0 0 20 20 20 20
+	//c = 0, 메모리 10이면
+	//10 10 10 40 
+	//즉 c 인덱스부터 값[index - c] + > 기존값인 경우 
+
 	cin >> n >> m;
+	process_list = vector<Process>(n);
 
-	vector<int> memory(n + 1);
-	vector<int> cost(n + 1);
-
-
-	for (i = 1; i <= n; i++) //n개
+	for (int i = 0; i < n; i++)
 	{
-		cin >> memory[i];
-	}
-	for (i = 1; i <= n; i++) //n개
-	{
-		cin >> cost[i];
-		sum += cost[i];
+		cin >> process_list[i].memory;
 	}
 
-	//i는 n개수, j는 cost, 배열 내용은 memory
-	for (i = 1; i < memory.size(); i++)
+	for (int i = 0; i < n; i++)
 	{
-		for (j = 0; j <= sum; j++)
+		cin >> process_list[i].cost;
+		sum += process_list[i].cost;
+		
+	}
+	dp = vector<int>(sum+ 1, 0);
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = sum; j >= process_list[i].cost; j--)
 		{
-			if (cost[i] <= j) {
-				dp[i][j] = max(memory[i] + dp[i - 1][j - cost[i]], dp[i - 1][j]);
-			}
-			else {
-				dp[i][j] = dp[i - 1][j];
+			int value = dp[j - process_list[i].cost] + process_list[i].memory;
+			if (value > dp[j]) {
+				dp[j] = value;
 			}
 		}
 	}
 
-	i--;
-	for (j = 0; j <= sum; j++)
+	for (int i = 0; i <= sum; i++)
 	{
-		if (m <= dp[i][j]) {
-			cout << j << '\n';
-			break;
+		if (dp[i] >= m)
+		{
+			cout << i << '\n';
+			return 0;
 		}
 	}
 }
