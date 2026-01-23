957d1145efe7129f56fc32648998f7ecb11aa0b6
gestgest <87427692+gestgest@users.noreply.github.com>
2026-01-24 01:13:03 +0900

    [Gold III] Title: 별자리 만들기, Time: 0 ms, Memory: 2300 KB -BaekjoonHub

--- 백준/Gold/4386. 별자리 만들기/별자리 만들기.cc
+++ 백준/Gold/4386. 별자리 만들기/별자리 만들기.cc
@@ -1,111 +1,111 @@
 #include <iostream>
+#include <vector>
 #include <cmath>
 #include <queue>
 
-#define INF 2147483647
-
 using namespace std;
 
+#define INF 2147483647
 
+//그래프?를 가장한 유니온
 class Point {
 public:
-	float x;
-	float y;
-	int index;
-	Point(float x, float y, int index) {
-		this->x = x;
-		this->y = y;
-		this->index = index;
+	double x;
+	double y;
+	int union_index;
+	Point(int union_index) 
+	{
+		this->union_index = union_index;
 	}
+	Point() {}
+
 };
 
+vector<Point> points;
 
-float getDis(Point& p1, Point& p2) {
-	return sqrtf((p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y));
+int find_union(int index)
+{
+	if (points[index].union_index == index) return index;
+	return points[index].union_index = find_union(points[index].union_index);
 }
+
+void add_union(int a, int b)
+{
+	a = find_union(a);
+	b = find_union(b);
+
+	if (a > b)
+		points[a].union_index = b;
+	else
+		points[b].union_index = a;
+}
+
+//add
+//
 
 class Edge {
 public:
-	float dis;
-	int index1;
-	int index2;
+	int start_index;
+	int end_index;
+	double dis;
 
-	Edge(float dis, int index1, int index2) {
-		this->dis = dis;
-		this->index1 = index1;
-		this->index2 = index2;
+	Edge(int start_index, int end_index, Point & a, Point & b)
+	{
+		this->start_index = start_index;
+		this->end_index = end_index;
+
+		dis = sqrt((a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y));
 	}
-
-	const bool operator< (const Edge& edge) const {
+	bool operator < (const Edge& edge) const & 
+	{
 		return this->dis > edge.dis;
 	}
 };
-
-
-int find_union(vector<Point>& points, int n)
-{
-	if (points[n].index == n) return n;
-	return points[n].index = find_union(points, points[n].index);
-}
-
-void insert_union(vector<Point>& points, int a, int b)
-{
-	a = find_union(points, a);
-	b = find_union(points, b);
-
-	if (a < b) {
-		points[b].index = a;
-	}
-	else {
-		points[a].index = b;
-	}
-}
-
 
 int main()
 {
 	cin.tie(NULL);
 	ios_base::sync_with_stdio(false);
-	cout.setf(ios::fixed); //정수도 소수점 아래 처리
-	cout.precision(2); //cout의 소수점 범위를 10자리까지 늘린다. 4은 3.1415
 
 	int n;
-	float x, y;
-	float sum = 0;
+	double sum = 0;
 	cin >> n;
+	priority_queue<Edge> pq;
 
-	priority_queue<Edge> pq_edge;
-	vector<Point> points;
-
-	//n^2 짜리 우선순위 큐
+	//모든 edge를 만들자, 시작 인덱스, 도착 인덱스, dis
+	//그리고 우선순위 큐?
 	for (int i = 0; i < n; i++)
 	{
-		cin >> x >> y;
-		points.push_back(Point(x, y, i));
+		Point p(i);
+		cin >> p.x >> p.y;
+		points.push_back(p);
+	}
 
-		//우선순위에 넣어 버리기
-		for (int j = 0; j < i; j++)
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = i + 1; j < n; j++)
 		{
-			pq_edge.push(Edge(getDis(points[i], points[j]), i, j));
+			Edge edge(i, j, points[i], points[j]);
+			pq.push(edge);
 		}
 	}
 
-	while (true)
+	while (!pq.empty())
 	{
-		if (pq_edge.empty())
-		{
-			break;
-		}
-		Edge edge = pq_edge.top();
-		pq_edge.pop();
+		Edge edge = pq.top();
+		pq.pop();
 
-		//유니온 탐색 (만약 다르다면)
-		if (find_union(points, edge.index1) != find_union(points, edge.index2))
+		//다른 범주
+		points[edge.start_index].union_index = find_union(points[edge.start_index].union_index);
+		points[edge.end_index].union_index = find_union(points[edge.end_index].union_index);
+
+		if (points[edge.start_index].union_index != points[edge.end_index].union_index)
 		{
-			insert_union(points, edge.index1, edge.index2);
+			add_union(edge.start_index, edge.end_index);
 			sum += edge.dis;
 		}
 	}
-
 	cout << sum << '\n';
+
+	//지나간 길(isstart, isend
 }
